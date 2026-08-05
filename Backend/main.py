4467fb2d1bd8057d98bae98654afb7ff4a77fdc9
Manish Rai <manishrai80008@gmail.com>
2026-08-05 13:38:26 +0530

    Defined Backend Schema

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,3 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
-
-class ex(BaseModel):
-  id: int
-  name: str
-  passw: str
-
-@app.post("/")
-def login(cred: ex):
-  return "done"
