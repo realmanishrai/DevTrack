8574a92dd9620e03938485c8d82ad275b077bd58
Manish Rai <manishrai80008@gmail.com>
2026-08-15 15:38:53 +0530

    added activitylogs endpoint

--- Backend/app/routers/dashboard.py
+++ Backend/app/routers/dashboard.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schema import GetTasks
-from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Task, Room
 from app import models, schema
